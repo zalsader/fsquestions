b6b40bf9ff209fa1ae5beaf0ac9a6640f148edb5
Zuhair AlSader <zalsader@uwaterloo.ca>
2018-03-31 19:33:12 -0400

    Update tally csv

--- queries/updatetally.sql
+++ queries/updatetally.sql
@@ -1,1 +1,2 @@
-update fsfunction f set tally = (select count(distinct fp.postId) from fsfunctionposts fp where fp.fsId = f.Id)
+update fsfunction f set tally = (select count(distinct fp.postId) from fsfunctionposts fp where fp.fsId = f.Id);
+select name, tally from fsfunction;

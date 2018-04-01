5c7bb7b506018acecd4db221664f8d13bcb3bece
Zuhair AlSader <zalsader@uwaterloo.ca>
2018-04-01 03:39:56 -0400

    Change coverage by time

--- scripts/1-3-draw-coverage-by-time.py
+++ scripts/1-3-draw-coverage-by-time.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import matplotlib
+from matplotlib.ticker import FuncFormatter
 import squarify
 import csv
 import numpy as np
+import datetime as dt
 
 FILENAME = 'csv/1-3-coveragebytime.csv'
+TOTALFN = 310 #TODO hardcoded
 
+def total_coverage(num):
+    return float(num)/TOTALFN
 
 def read_coverage_time(fname):
     dates = []
@@ -16,13 +22,11 @@ def read_coverage_time(fname):
     with open(fname, 'r') as csvfile:
         myreader = csv.DictReader(csvfile)
         for row in myreader:
-            #TODO calculate coverage
-            #dates.append(row['yearmonth'])
-            dates.append(row['yearmonthnum'])
-            coverage1.append(int(row['covered1']))
-            coverage2.append(int(row['covered2']))
-            coverage5.append(int(row['covered5']))
-            coverage10.append(int(row['covered10']))
+            dates.append(dt.datetime.strptime(row['yearmonth'],'%Y-%m').date())
+            coverage1.append(total_coverage(row['covered1']))
+            coverage2.append(total_coverage(row['covered2']))
+            coverage5.append(total_coverage(row['covered5']))
+            coverage10.append(total_coverage(row['covered10']))
     return (dates, coverage1, coverage2, coverage5, coverage10)
 
 if __name__ == '__main__':
@@ -38,5 +42,9 @@ if __name__ == '__main__':
     ax.fill_between(dates, 0, coverage2, color=cmap(norm(2)))
     ax.fill_between(dates, 0, coverage5, color=cmap(norm(3)))
     ax.fill_between(dates, 0, coverage10, color=cmap(norm(4)))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+    ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
+    ax.set_ylim([0,0.4])
+    plt.margins(0)
     #plt.show()
     plt.savefig('figures/1-3-coverage-by-time.pdf', bbox_inches='tight')

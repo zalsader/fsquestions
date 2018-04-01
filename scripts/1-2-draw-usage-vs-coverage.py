73792b392902cc2e61609518e62238258fef0a9d
Zuhair AlSader <zalsader@uwaterloo.ca>
2018-04-01 11:51:05 -0400

    Add trendlind and calculate slope

--- scripts/1-2-draw-usage-vs-coverage.py
+++ scripts/1-2-draw-usage-vs-coverage.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib
+import numpy as np
 import squarify
 import csv
 
@@ -23,12 +24,25 @@ if __name__ == '__main__':
     xdata = []
     ydata = []
     for k in fscounts:
+        if fscounts[k] == 0:
+            continue
         xdata.append(ucounts[k])
         ydata.append(fscounts[k])
-    plt.plot(xdata, ydata, 'ko')
+    plt.plot(xdata, ydata, 'ko', alpha=0.5)
+    logx = np.log(xdata)
+    logy = np.log(ydata)
+    coof = np.polyfit(logx, logy, 1)
+    p = np.poly1d(coof)
+    yfit = lambda x: np.exp(p(np.log(x)))
+    orderedx = np.sort(xdata)
+    plt.plot(orderedx,yfit(orderedx), 'k--')
+    # the line equation:
+    print "y=%.6fx+(%.6f)"%(coof[0],coof[1])
+    # y=0.541294x+(-6.112327)
     plt.yscale('log')
     plt.xscale('log')
+    plt.ylim([0.75,500])
     plt.xlabel('Number of Github References')
     plt.ylabel('Number of Questions Referenced')
-    #plt.show()
-    plt.savefig('figures/1-2-usage-vs-coverage.pdf', bbox_inches='tight')
+    plt.show()
+    #plt.savefig('figures/1-2-usage-vs-coverage.pdf', bbox_inches='tight')
